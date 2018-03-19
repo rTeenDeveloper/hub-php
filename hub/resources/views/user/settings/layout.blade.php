30cb804a27f6533e25f18f1fc1c8a565fdfbfd09
Peter <dante383@protonmail.com>
2018-03-19 02:36:10 +0100

    Dismissible alert

--- hub/resources/views/user/settings/layout.blade.php
+++ hub/resources/views/user/settings/layout.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <div class="container">
     @if(Session::has('message'))
-        <div class="alert alert-success alert-dismissible">
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             {{ Session::get('message') }}
         </div>
     @endif
